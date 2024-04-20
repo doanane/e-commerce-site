8abc03e07c8c84b78e4fa9d8f7f8e5ccffad6059
Desond Anane <doanane@st.ug.edu.gh>
2024-04-20 04:01:15 +0000

    mysql database setup

--- index.php
+++ index.php
@@ -1,22 +1,35 @@
 <?php require "includes/header.php"; ?>
+<?php require "config/config.php"; ?>
+<?php
+
+$rows = $conn->query("SELECT * FROM products WHERE status = 1");
+$rows->execute();
+$allRows = $rows->fetchAll(PDO::FETCH_OBJ);
+?>
+
+
 <div class="row mt-5">
+    <?php foreach ($allRows as $product) : ?>
     <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1">
         <div class="card">
-            <img height="213px" class="card-img-top" src="images/node.png">
+            <img height="213px" class="card-img-top" src="images/<?php echo $product->image; ?>">
             <div class="card-body">
-                <h5 class="d-inline"><b>Node Basics</b> </h5>
                 <h5 class="d-inline">
-                    <div class="text-muted d-inline">($10/item)</div>
+                    <b<?php echo $product->price; ?>< /b>
                 </h5>
-                <p>Monotonectally enable customized
-                    growth strategies and 24/7 portals. functional opportunities. </p>
-                <a href="#" class="btn btn-primary w-100 rounded my-2"> More<i class="fas fa-arrow-right"></i>
+                <h5 class="d-inline">
+                    <div class="text-muted d-inline">($<?php echo $product->price; ?>/item)</div>
+                </h5>
+                <p><?php echo $product->description; ?></p>
+                <a href="<?php echo APPURL; ?>shopping/single.php?id=<?php echo $product->name; ?>"
+                    class="btn btn-primary w-100 rounded my-2"> More<i class="fas fa-arrow-right"></i>
                 </a>
 
             </div>
         </div>
     </div>
     <br>
+    <?php endforeach; ?>
     <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1 mb-5">
         <div class="card">
             <a href="http://localhost/bookstore/shopping/single.php"><img height="213px" class="card-img-top"
@@ -66,7 +79,7 @@
         </div>
     </div>
     <br>
-    <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1">
+    <!-- <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1">
         <div class="card">
             <img height="213px" class="card-img-top" src="images/html5.jpg">
             <div class="card-body">
@@ -97,7 +110,7 @@
                 </a>
             </div>
         </div>
-    </div>
+    </div> -->
 </div>
 
 <?php require "includes/footer.php"; ?>
